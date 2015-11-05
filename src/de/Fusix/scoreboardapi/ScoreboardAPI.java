d6e72fa944abc9bdbbfb0d096d92dec85d85c8c3
Fusix <Fusix@users.noreply.github.com>
2015-11-05 17:26:26 +0100

    Indention update

--- src/de/Fusix/scoreboardapi/ScoreboardAPI.java
+++ src/de/Fusix/scoreboardapi/ScoreboardAPI.java
@@ -6,22 +6,22 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ScoreboardAPI extends JavaPlugin {
-	
-	private static ArrayList<de.Fusix.scoreboardapi.Scoreboard> scoreboards = new ArrayList<>();
-	
-	public static Scoreboard newScoreboard(String type, String title, int duration) {
-		Scoreboard sb = new de.Fusix.scoreboardapi.Scoreboard(type, title, duration);
-		scoreboards.add(sb);
-		return sb;
-	}
-	
-	public static void setActiveOnPlayer(Player player, String scoreboardName) {
-		for(int i = 0; i < scoreboards.size(); i++) {
-			if(scoreboards.get(i).getName().equals(scoreboardName)) {
-				scoreboards.get(i).apply(player);
-				break;
-			}
+    
+    private static ArrayList<de.Fusix.scoreboardapi.Scoreboard> scoreboards = new ArrayList<>();
+    
+    public static Scoreboard newScoreboard(String type, String title, int duration) {
+        Scoreboard sb = new de.Fusix.scoreboardapi.Scoreboard(type, title, duration);
+        scoreboards.add(sb);
+        return sb;
+    }
+    
+    public static void setActiveOnPlayer(Player player, String scoreboardName) {
+        for(int i = 0; i < scoreboards.size(); i++) {
+            if(scoreboards.get(i).getName().equals(scoreboardName)) {
+                scoreboards.get(i).apply(player);
+                break;
+            }
         }
-	}
-	
+    }
+    
 }
