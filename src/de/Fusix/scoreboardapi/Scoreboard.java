28e9d741eac6c43920156633d2396cc35577ccce
Fusix <Fusix@users.noreply.github.com>
2015-11-05 21:17:53 +0100

    Update Scoreboard.java

--- src/de/Fusix/scoreboardapi/Scoreboard.java
+++ src/de/Fusix/scoreboardapi/Scoreboard.java
@@ -12,16 +12,49 @@ public class Scoreboard {
     private org.bukkit.scoreboard.Scoreboard sb;
     private Objective o;
     private String title;
-    private String type;
     private HashMap<String, Integer> scores = new HashMap<>();
     
     public Scoreboard(String type, String title, int duration) {
         sb = Bukkit.getScoreboardManager().getNewScoreboard();
         o = sb.registerNewObjective(title, type);
         this.title = title;
-        this.type = type;
         o.setDisplayName(title);
         o.setDisplaySlot(DisplaySlot.SIDEBAR);
+        if(duration < -1) {
+            throw new IllegalArgumentException("duration must be -1 or greater");
+        } else if(duration != -1) {
+            Bukkit.getScheduler().scheduleSyncDelayedTask(ScoreboardAPI.getInstance(), new Runnable() {
+                @Override
+                public void run() {
+                    for(Player p : Bukkit.getServer().getOnlinePlayers()) {
+                        if(p.getScoreboard().equals(sb)) {
+                            p.setScoreboard(null);
+                        }
+                    }
+                }
+            }, duration*20);
+        }
+    }
+    
+    public static enum ScoreboardPreset {
+        Health, Health2
+    }
+    
+    public void loadPreset(ScoreboardPreset sbp) {
+        switch(sbp) {
+            case Health:
+                o.unregister();
+                o = sb.registerNewObjective(title, "health");
+                o.setDisplaySlot(DisplaySlot.BELOW_NAME);
+                o.setDisplayName("§c❤");
+                break;
+            case Health2:
+                o.unregister();
+                o = sb.registerNewObjective(title, "health");
+                o.setDisplaySlot(DisplaySlot.BELOW_NAME);
+                o.setDisplayName("/ 20");
+                break;
+        }
     }
     
     public void setTitle(String title) {
@@ -29,27 +62,28 @@ public class Scoreboard {
     }
 
     public void setType(String type) {
+        o.unregister();
         o = sb.registerNewObjective(title, type);
     }
     
-    /*public static void setDuration() {
+    public static void setDuration() {
         
-    }*/
+    }
 
     public void setScore(int score, String scoreName) {
-        o.getScore(scoreName).setScore(score);
-        scores.put(scoreName, score);
+        o.getScore(scoreName.replace('&', '§')).setScore(score);
+        scores.put(scoreName.replace('&', '§'), score);
     }
     
     public void removeScore(String scoreName) {
-        scores.remove(scoreName);
-        sb.resetScores(scoreName);
+        scores.remove(scoreName.replace('&', '§'));
+        sb.resetScores(scoreName.replace('&', '§'));
     }
     
     public void removeAllScores() {
-		for(String key : scores.keySet()) sb.resetScores(key);
-		scores = new HashMap<>();
-	}
+        for(String key : scores.keySet()) sb.resetScores(key);
+        scores = new HashMap<>();
+    }
     
     public String getName() {
         return o.getDisplayName();
