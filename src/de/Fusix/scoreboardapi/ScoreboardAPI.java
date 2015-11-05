79127925385d160e7c6002a0f716202c3c90503f
Fusix <Fusix@users.noreply.github.com>
2015-11-05 21:18:09 +0100

    Update ScoreboardAPI.java

--- src/de/Fusix/scoreboardapi/ScoreboardAPI.java
+++ src/de/Fusix/scoreboardapi/ScoreboardAPI.java
@@ -6,28 +6,28 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ScoreboardAPI extends JavaPlugin {
-	
-	private static ArrayList<de.Fusix.scoreboardapi.Scoreboard> scoreboards = new ArrayList<>();
-	private static ScoreboardAPI scorebaordapi;
-	
-	public static Scoreboard newScoreboard(String type, String title, int duration) {
-		Scoreboard sb = new de.Fusix.scoreboardapi.Scoreboard(type, title, duration);
-		scoreboards.add(sb);
-		return sb;
-	}
-	
-	public static void setActiveOnPlayer(Player player, String scoreboardName) {
-		for(int i = 0; i < scoreboards.size(); i++) {
-			if(scoreboards.get(i).getName().equals(scoreboardName)) {
-				scoreboards.get(i).apply(player);
-				break;
-			}
-		}
-	}
-	
-	public static synchronized ScoreboardAPI getInstance() {
-		if (scorebaordapi == null) scorebaordapi = new ScoreboardAPI();
-		return scorebaordapi;
-	}
-	
+    
+    private static ArrayList<de.Fusix.scoreboardapi.Scoreboard> scoreboards = new ArrayList<>();
+    private static ScoreboardAPI scorebaordapi;
+    
+    public static Scoreboard newScoreboard(String type, String title, int duration) {
+        Scoreboard sb = new de.Fusix.scoreboardapi.Scoreboard(type, title, duration);
+        scoreboards.add(sb);
+        return sb;
+    }
+    
+    public static void setActiveOnPlayer(Player player, String scoreboardName) {
+        for(int i = 0; i < scoreboards.size(); i++) {
+            if(scoreboards.get(i).getName().equals(scoreboardName)) {
+                scoreboards.get(i).apply(player);
+                break;
+            }
+        }
+    }
+    
+    public static synchronized ScoreboardAPI getInstance() {
+        if (scorebaordapi == null) scorebaordapi = new ScoreboardAPI();
+        return scorebaordapi;
+    }
+    
 }
