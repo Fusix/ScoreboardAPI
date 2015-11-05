84aadb65e8f0a64f2b69244fa675e8eaeb33d589
Fusix <Fusix@users.noreply.github.com>
2015-11-05 17:26:52 +0100

    Update Scoreboard.java

--- src/de/Fusix/scoreboardapi/Scoreboard.java
+++ src/de/Fusix/scoreboardapi/Scoreboard.java
@@ -8,50 +8,50 @@ import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
 
 public class Scoreboard {
-	
-	private org.bukkit.scoreboard.Scoreboard sb;
-	private Objective o;
-	private String title;
-	private String type;
-	private HashMap<String, Integer> scores = new HashMap<>();
-	
-	public Scoreboard(String type, String title, int duration) {
-		sb = Bukkit.getScoreboardManager().getNewScoreboard();
-		o = sb.registerNewObjective(title, type);
-		this.title = title;
-		this.type = type;
-		o.setDisplayName(title);
-		o.setDisplaySlot(DisplaySlot.SIDEBAR);
-	}
-	
-	public void setTitle(String title) {
-		o.setDisplayName(title);
-	}
+    
+    private org.bukkit.scoreboard.Scoreboard sb;
+    private Objective o;
+    private String title;
+    private String type;
+    private HashMap<String, Integer> scores = new HashMap<>();
+    
+    public Scoreboard(String type, String title, int duration) {
+        sb = Bukkit.getScoreboardManager().getNewScoreboard();
+        o = sb.registerNewObjective(title, type);
+        this.title = title;
+        this.type = type;
+        o.setDisplayName(title);
+        o.setDisplaySlot(DisplaySlot.SIDEBAR);
+    }
+    
+    public void setTitle(String title) {
+        o.setDisplayName(title);
+    }
 
-	public void setType(String type) {
-		o = sb.registerNewObjective(title, type);
-	}
-	
-	/*public static void setDuration() {
-		
-	}*/
+    public void setType(String type) {
+        o = sb.registerNewObjective(title, type);
+    }
+    
+    /*public static void setDuration() {
+        
+    }*/
 
-	public void setScore(int score, String scoreName) {
-		o.getScore(scoreName).setScore(score);
-		scores.put(scoreName, score);
-	}
-	
-	public void removeScore(String scoreName) {
-		scores.remove(scoreName);
-		sb.resetScores(scoreName);
-	}
-	
-	public String getName() {
-		return o.getDisplayName();
-	}
-	
-	public void apply(Player player) {
-		player.setScoreboard(sb);
-	}
+    public void setScore(int score, String scoreName) {
+        o.getScore(scoreName).setScore(score);
+        scores.put(scoreName, score);
+    }
+    
+    public void removeScore(String scoreName) {
+        scores.remove(scoreName);
+        sb.resetScores(scoreName);
+    }
+    
+    public String getName() {
+        return o.getDisplayName();
+    }
+    
+    public void apply(Player player) {
+        player.setScoreboard(sb);
+    }
 
 }
